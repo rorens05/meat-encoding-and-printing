7b82f185f23cd9aa5642fdd85f4241ca33a5e4bd
Laurence Buatista <rorens05@gmail.com>
2019-09-18 15:45:43 +0800

    pre final

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { thisExpression } from '@babel/types';
 
 export default class App extends Component {
 	constructor(props) {
@@ -6,11 +7,11 @@ export default class App extends Component {
 		this.state = {
 			imported_by: '',
 			from: '',
-			description: '',
-			evqmilc: '',
+			meat_description: '',
+			evqmilc_no: '',
 			container_no: '',
 			volume: '',
-			brand: '',
+			brand_name: '',
 			production_date: '',
 			expiry_date: '',
 			fme_name: '',
@@ -19,55 +20,124 @@ export default class App extends Component {
 			lot_no: '',
 			buyer: '',
 			destination: '',
-			withdrawal: '',
+			withdrawal_date: '',
 			mtv_no: '',
 			day: '',
 			month: '',
-			time: ''
+			time: '',
+			received_by: ''
 		};
 	}
 
+	input = (props) => {
+		return (
+			<div className='input-container'>
+				<label>{props.label}</label>
+				<input type='text' onChange={this.onChange} name={props.name} placeholder={props.label} />
+			</div>
+		);
+	};
+
+	onChange = (event) => {
+		this.setState({ [event.target.name]: event.target.value }, () => {
+			console.log(this.state);
+		});
+	};
+
+	renderAllKey = (call) => {
+		let Input = this.input;
+		return Object.keys(this.state).map((key) => {
+			console.log(key);
+			return <Input name={key} key={key} value={this.state[key]} label={key.split('_').join(' ')} />;
+		});
+	};
+
 	render() {
 		return (
+			// imported_by: '',
+			// from: '',
+			// meat_description: '',
+			// evqmilc_no: '',
+			// container_no: '',
+			// volume: '',
+			// brand_name: '',
+			// production_date: '',
+			// expiry_date: '',
+			// fme_name: '',
+			// fme_no: '',
+			// paking_size: '',
+			// lot_no: '',
+			// buyer: '',
+			// destination: '',
+			// withdrawal_date: '',
+			// mtv_no: '',
+			// day: '',
+			// month: '',
+			// time: ''
 			<div>
 				<div className='outer-div'>
 					<div className='inner-div'>
-						<p className='imported-by'>imported by</p>
-						<span className='from'>From nameasdfsadfasdf</span>
+						<p className='imported-by'>{this.state.imported_by}</p>
+						<span className='from'>{this.state.from}</span>
 						<br />
 						<div className='list-containers'>
-							<span>Meat Description</span>
-							<span>eVQMILC</span>
-							<span>Container No</span>
-							<span>Volume</span>
-							<span>Brand name</span>
-							<span>production date</span>
-							<span>expiry date</span>
-							<span>fme name</span>
-							<span>fme no</span>
-							<span>packing size</span>
-							<span>log no</span>
+							<span>{this.state.meat_description}</span>
+							<span>{this.state.evqmilc_no}</span>
+							<span>{this.state.container_no}</span>
+							<span>{this.state.volume}</span>
+							<span>{this.state.brand_name}</span>
+							<span>{this.state.production_date}</span>
+							<span>{this.state.expiry_date}</span>
+							<span>{this.state.fme_name}</span>
+							<span>{this.state.fme_no}</span>
+							<span>{this.state.paking_size}</span>
+							<span>{this.state.lot_no}</span>
 						</div>
 						<br />
 
 						<div className='list-containers-bottom'>
-							<span>Buyer/ End User:</span>
-							<span>Destination</span>
-							<span>withdrawal</span>
-							<span>MTV No</span>
+							<span>{this.state.buyer}</span>
+							<span>{this.state.destination}</span>
+							<span>{this.state.withdrawal_date}</span>
+							<span>{this.state.mtv_no}</span>
 							<span>
-								<p style={{ marginLeft: 68 }}>12th</p>
-								<p style={{ marginLeft: 68 }}> September</p>
-								<p style={{ marginLeft: 68 }}>4321421</p>
+								<p style={{ marginLeft: 68 }}>{this.state.day}</p>
+								<p style={{ marginLeft: 68 }}> {this.state.month}</p>
+								<p style={{ marginLeft: 68 }}>{this.state.time}</p>
 							</span>
 						</div>
-						<span className='received'>Recieved By:</span>
+						<span className='received'>{this.state.received_by}</span>
 					</div>
 				</div>
 
 				<div className='display'>
 					<h1>Encoding Program</h1>
-					<form />
+					<form>
+						{this.renderAllKey()}
+						<input
+							className='btn btn-reset'
+							value='Reset'
+							type='submit'
+							onClick={(e) => {
+								e.preventDefault();
+
+								window.location.reload();
+							}}
+						/>
+						<input
+							className='btn btn-print'
+							value='Print'
+							type='submit'
+							onClick={(e) => {
+								e.preventDefault();
+								window.print();
+							}}
+						/>
+					</form>
+					<br />
+					<br />
+					<br />
+					<br />
 				</div>
 			</div>
 		);
